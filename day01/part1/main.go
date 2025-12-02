c9c3a599f25456bd1a7aa2efeae9cc9c1c078d86
talayhan <samet.talayhan@gmail.com>
2025-12-01 20:05:39 -0800

    Add day01/part1

---
+++ day01/part1/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	const filename = "input.txt"
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Errorf("failed to open the file %s: %w", filename, err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	lineNumber := 0
+
+	startPos := 50
+	zeroCount := 0
+
+	for scanner.Scan() {
+		lineNumber++
+		line := scanner.Text()
+
+		direction := line[0] // 'L' or 'R'
+
+		distanceStr := line[1:]
+		distance, err := strconv.Atoi(distanceStr)
+		if err != nil {
+			fmt.Printf("Couldn't parse distance %s as integer %v\n", distanceStr, err)
+			continue
+		}
+
+		if direction == 'L' {
+			startPos = (startPos - distance) % 100
+			if startPos < 0 {
+				startPos += 100
+			}
+		} else if direction == 'R' {
+			startPos = (startPos + distance) % 100
+		}
+
+		if startPos == 0 {
+			zeroCount++
+		}
+	}
+	fmt.Println("Total zero count: ", zeroCount)
+
+	if err := scanner.Err(); err != nil {
+		fmt.Errorf("file scanning failed: %w", err)
+	}
+}
